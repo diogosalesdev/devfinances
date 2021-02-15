ca37b15f31889108ba7869f73240c3154163648a
Diogo Sales <diogosalesdev@gmail.com>
2021-02-15 18:19:28 -0300

    Creating methods for app

--- devfinance/scripts.js
+++ devfinance/scripts.js
@@ -4,7 +4,8 @@ const Modal = {
     //Adicionar a classe active ao modal
     document
       .querySelector('.modal-overlay')
-      .classList.add('active')
+      .classList
+      .add('active')
   },
 
   close() {
@@ -12,6 +13,55 @@ const Modal = {
     //Remover a classe active do modal
     document
       .querySelector('.modal-overlay')
-      .classList.remove('active')
+      .classList
+      .remove('active')
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 2,
+    description: 'website',
+    amount: 500000,
+    date: '23/01/2021'
+  },
+  {
+    id: 3,
+    description: 'internet',
+    amount: -20000,
+    date: '23/01/2021'
+  }
+]
+
+const Transaction = {
+  incomes() {
+    // somar as entradas
+  },
+  expenses() {
+    // somar as saídas
+  },
+  total() {
+    // entradas - saídas
+  }
+}
+
+const DOM = {
+  innerHTMLTransaction() {
+    const html = `
+    <tr>
+      <td class="description">Luz</td>
+      <td class="expense">- R$ 500,00</td>
+      <td class="date">22/01/2021</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação">
+      </td>
+    </tr>
+    `
   }
 }
